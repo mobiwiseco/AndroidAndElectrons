980625c264598e8f88077b6de16645ebd09313a8
iammert <mert@mobiwise.co>
2015-11-12 13:28:28 +0200

    Added resource binding to butterknife module

--- electronbutterknife/src/main/java/co/mobiwise/electronbutterknife/ButterknifeActivity.java
+++ electronbutterknife/src/main/java/co/mobiwise/electronbutterknife/ButterknifeActivity.java
@@ -1,12 +1,17 @@
 package co.mobiwise.electronbutterknife;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import butterknife.Bind;
+import butterknife.BindColor;
+import butterknife.BindDimen;
+import butterknife.BindDrawable;
+import butterknife.BindString;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -17,6 +22,18 @@ public class ButterknifeActivity extends Activity{
 
     @Bind(R.id.button)
     Button button;
+
+    @BindString(R.string.test)
+    String injectedString;
+
+    @BindColor(R.color.testColor)
+    int injectedColor;
+
+    @BindDrawable(android.R.drawable.btn_plus)
+    Drawable injectedDrawable;
+
+    @BindDimen(R.dimen.testDimen)
+    float injectedDimen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
